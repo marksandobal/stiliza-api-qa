6008efb343a6136d11a1c5d535a86a00e6a85c5e
Arturo Hernandez <marksandobal@gmail.com>
2026-01-15 19:44:23 -0600

    Add new environment file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
   config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    config.logger = ActiveSupport::TaggedLogging.logger(STDOUT)
+  end
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!).
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
@@ -63,6 +66,7 @@ Rails.application.configure do
     user_name: ENV.fetch("USER_MAILER_SENDER"),
     password: ENV.fetch("USER_MAILER_PASSWORD"),
     authentication: :plain,
+    domain: ENV.fetch("DOMAIN_HOST"),
     enable_starttls_auto: true
   }
 
